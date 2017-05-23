409cd626526d5b26e75e4cfc0ecadd76cc5d7d6c
Kyahra <sofiacbahamonde@gmail.com>
2017-05-23 17:40:35 +0100

    new project

---
+++ Beer Pong/desktop/src/com/mygdx/beerpong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.beerpong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.beerpong.BeerPong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeerPong(), config);
+	}
+}
