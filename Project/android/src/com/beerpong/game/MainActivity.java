33fa30737a00a4a6eb5b808316c31e094c6f0f3f
kyahra <up201504570@FEUPSIG.FE.UP.PT>
2017-06-02 17:33:20 +0100

    revert commits

--- Project/android/src/com/beerpong/game/MainActivity.java
+++ Project/android/src/com/beerpong/game/MainActivity.java
@@ -15,7 +15,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
         setContentView(R.layout.mainlayout);
         findViewById(R.id.startGame).setOnClickListener(this);
         findViewById(R.id.exitButton).setOnClickListener(this);
