d3d3ea8b84c77ed5c1610673b189dedf74517f79
Kyahra <sofiacbahamonde@gmail.com>
2017-06-02 23:16:44 +0100

    facebook log in working

--- Project/android/src/com/beerpong/game/MainActivity.java
+++ Project/android/src/com/beerpong/game/MainActivity.java
@@ -6,15 +6,25 @@ import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+
 /**
  * Created by Daniel on 31/05/2017.
  */
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    LoginButton loginButton;
+    CallbackManager callBackManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         getWindow().getDecorView().setSystemUiVisibility (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -25,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.mainlayout);
         findViewById(R.id.startGame).setOnClickListener(this);
         findViewById(R.id.exitButton).setOnClickListener(this);
+
+        loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+
+
 
     }
 
@@ -38,5 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.exitButton:
                 System.exit(0);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 }
