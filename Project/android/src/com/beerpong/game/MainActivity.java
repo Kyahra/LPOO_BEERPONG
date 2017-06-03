59188b2ab9579f9d5572d161d1543bb5d1eb556c
dolfander <daniel.pmachado@hotmail.com>
2017-06-03 03:21:35 +0100

    new layout

--- Project/android/src/com/beerpong/game/MainActivity.java
+++ Project/android/src/com/beerpong/game/MainActivity.java
@@ -36,13 +36,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.startGame).setOnClickListener(this);
         findViewById(R.id.exitButton).setOnClickListener(this);
 
+
         loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+        loginButton.setScaleX(2);
+        loginButton.setScaleY(2);
 
 
 
     }
 
-
+/*
     @Override
     public void onClick(View v) {
         switch(v.getId()){
@@ -53,6 +56,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 System.exit(0);
         }
     }
+*/
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.startGame:
+
+                setContentView(R.layout.levelayout);
+                findViewById(R.id.easyButton).setOnClickListener(this);
+                findViewById(R.id.normalButton).setOnClickListener(this);
+                findViewById(R.id.difficultButton).setOnClickListener(this);
+
+                break;
+            case R.id.easyButton:
+                startActivity(new Intent(this, AndroidLauncher.class));
+                break;
+            case R.id.normalButton:
+                startActivity(new Intent(this, AndroidLauncher.class));
+                break;
+            case R.id.difficultButton:
+                startActivity(new Intent(this, AndroidLauncher.class));
+                break;
+            case R.id.exitButton:
+                System.exit(0);
+        }
+    }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
